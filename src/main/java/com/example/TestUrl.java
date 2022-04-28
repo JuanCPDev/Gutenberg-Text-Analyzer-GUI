91eb7f412ff3eaaa3a6c34a00632824823df89e5
JuanCPDev <86272014+JuanCPDev@users.noreply.github.com>
2022-04-27 20:43:05 -0400

    Add files via upload

---
+++ src/main/java/com/example/TestUrl.java
@@ -0,0 +1,14 @@
+package textanalyzertestomg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestUrl {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
